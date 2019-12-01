bwnyasse/flutter-movie-deep-dive-test

core(*): implement movie db without any tests

lib/src/app.dart
@@ -1,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_movie_deep_dive_test/src/blocs/blocs.dart';
+import 'package:flutter_movie_deep_dive_test/src/services/services.dart';
+import 'package:flutter_movie_deep_dive_test/src/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Devfest London',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: BlocProvider(
+        builder: (context) => AppBloc(
+          service: Provider.of<AppService>(
+            context,
+            listen: false,
+          ),
+        ),
+        child: MyHomePage(title: 'Movies - Devfest London'),
+      ),
+    );
+  }
+}

lib/src/services/app_service.dart
@@ -1,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter_movie_deep_dive_test/src/models/models.dart';
+import 'package:http/http.dart' show Client;
+
+class LoadMoviesException implements Exception {
+  final message;
+
+  LoadMoviesException(this.message);
+}
+
+class AppService {
+  final Client client;
+
+  AppService(this.client);
+
+  Future<MoviesResponse> loadMovies() async {
+    final apiKey = '4205ec1d93b1e3465f636f0956a98c64';
+    final api = 'https://api.themoviedb.org/3';
+    final urlPath = 'movie/now_playing';
+    final path = '$api/$urlPath?api_key=$apiKey&language=en-US';
+
+    // appel asynchrone
+    final response = await client.get(path);
+
+    if (response.statusCode != 200) {
+      throw LoadMoviesException('LoadMovies - Request Error: ${response.statusCode}');
+    }
+
+    // Décoder le contenu de la response ici
+    final data = json.decode(response.body);
+
+    return MoviesResponse.fromJson(data);
+  }
+}

bwnyasse/flutter-montreal

lib/src/app.dart
@@ -1,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_movie_deep_dive_test/src/blocs/blocs.dart';
+import 'package:flutter_movie_deep_dive_test/src/services/services.dart';
+import 'package:flutter_movie_deep_dive_test/src/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Montreal',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: BlocProvider(
+        builder: (context) => AppBloc(
+          service: Provider.of<AppService>(
+            context,
+            listen: false,
+          ),
+        ),
+        child: MyHomePage(title: 'Movies - Flutter Montreal'),
+      ),
+    );
+  }
+}

lib/src/services/app_service.dart
@@ -1,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter_movie_deep_dive_test/src/models/models.dart';
+import 'package:http/http.dart' show Client;
+
+class LoadMoviesException implements Exception {
+  final message;
+
+  LoadMoviesException(this.message);
+}
+
+class AppService {
+  final Client client;
+
+  AppService(this.client);
+
+  Future<MoviesResponse> loadMovies() async {
+    final apiKey = '4205ec1d93b1e3465f636f0956a98c64';
+    final api = 'https://api.themoviedb.org/3';
+    final urlPath = 'movie/now_playing';
+    final path = '$api/$urlPath?api_key=$apiKey&language=fr-CA';
+
+    // appel asynchrone
+    final response = await client.get(path);
+
+    if (response.statusCode != 200) {
+      throw LoadMoviesException('LoadMovies - Request Error: ${response.statusCode}');
+    }
+
+    // Décoder le contenu de la response ici
+    final data = json.decode(response.body);
+
+    return MoviesResponse.fromJson(data);
+  }
+}